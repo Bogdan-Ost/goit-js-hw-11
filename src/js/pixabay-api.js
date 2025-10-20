ae229069acbd28842905efe598a4dd3420813be1
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2025-10-20 20:09:45 +0300

    current number 2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-// Описаний у документації
-import iziToast from 'izitoast';
-// Додатковий імпорт стилів
-import 'izitoast/dist/css/iziToast.min.css';
-
 const KEY = '52797482-aafa905b189c4ff0c9ee358ec';
 
 function getImagesByQuery(query) {
@@ -20,13 +15,6 @@ function getImagesByQuery(query) {
     .then(({ data }) => {
       if (data.hits.length > 0) {
         return data;
-      } else {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-          // color: red,
-        });
       }
     })
     .catch(error => {});
--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import {
   hideLoader,
 } from './js/render-functions';
 
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
 const form = document.querySelector('.form');
 const input = form.elements['search-text'];
 form.addEventListener('submit', searchWord);
@@ -21,11 +26,19 @@ function searchWord(event) {
 
   getImagesByQuery(input.value)
     .then(data => {
-      if (data) {
+      if (data && data.hits) {
         createGallery(data.hits);
       }
+      throw new Error(response.status);
     })
-    .catch(error => console.log(error.message))
+    .catch(error =>
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        color: 'red',
+      })
+    )
     .finally(() => hideLoader());
 
   event.target.reset();
